0e2c51c3c44b4f9d64f560680237834e8c3d6f6c
Igor Polunin <garry.818@yandex.ru>
2023-03-26 15:56:45 +0300

    начало работы

---
+++ ImageFeedz/ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListCell.swift
+//  ImageFeedz
+//
+//  Created by Игорь Полунин on 26.03.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImageslistCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
--- ImageFeedz/ImagesList/ImagesListViewController.swift
+++ ImageFeedz/ImagesList/ImagesListViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
+    
     @IBOutlet private weak var tableView: UITableView!
     override func viewDidLoad() {
+        
+        print("fdkjkdkjf")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    private func configCell(for cell: ImageslistCell) {
+        
+    }
+    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -24,10 +30,14 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageslistCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImageslistCell else {
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        return imageListCell
     }
-    
-    
 }
 extension ImagesListViewController: UITableViewDelegate {
     
