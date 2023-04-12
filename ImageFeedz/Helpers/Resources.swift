06616a036610c545bb23d2bed119a7664b247989
Igor Polunin <garry.818@yandex.ru>
2023-04-12 22:02:26 +0300

    профайл сверстан кодом

---
+++ ImageFeedz/Helpers/Resources.swift
@@ -0,0 +1,14 @@
+//
+//  Resources.swift
+//  ImageFeedz
+//
+//  Created by Игорь Полунин on 12.04.2023.
+//
+
+import Foundation
+import UIKit
+//
+//enum ProfileImages {
+//    static var logoutButton = UIImage(named: "logoutButton")
+//    static var avatarImage = UIImage(named: "avatar")
+//}
